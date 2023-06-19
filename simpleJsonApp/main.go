14fa08b852be5ffe19e4a6a5dc4c0127c7f77682
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2023-06-19 15:27:20 -0500

    swagger setup done

--- simpleJsonApp/main.go
+++ simpleJsonApp/main.go
@@ -9,26 +9,24 @@ import (
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-//	@title			Simple Json App
-//	@version		0.1.0
-//	@description	This is a simple json app, apis of which will be fed into a crossplane provider.
-
-//	@license.name	Apache 2.0
-//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
-
-//	@host		localhost:8080
+// @title			Simple Json App
+// @version		0.1.0
+// @description	This is a simple json app, apis of which will be fed into a crossplane provider.
+// @license.name	Apache 2.0
+// @license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+// @host		localhost:8080
 // @accept 	json
 // @produce	json
-
 func main() {
 	router := gin.Default()
-	// docs.SwaggerInfo.BasePath = "/"
+	json := router.Group("/json")
+	docs.SwaggerInfo.BasePath = "/json"
 	docs.SwaggerInfo.Title = "Simple Json App"
-	router.POST("/record", crud.CreateRecord)
-	router.GET("/records", crud.GetRecords)
-	router.GET("/records/:name", crud.GetRecordsByName)
-	router.PUT("/record", crud.UpdateRecord)
-	router.DELETE("/record", crud.DeleteRecord)
+	json.POST("/record", crud.CreateRecord)
+	json.GET("/records", crud.GetRecords)
+	json.GET("/records/:name", crud.GetRecordsByName)
+	json.PUT("/record", crud.UpdateRecord)
+	json.DELETE("/record", crud.DeleteRecord)
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	router.Run("localhost:8080")
