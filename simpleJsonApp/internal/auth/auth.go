4907185ebad9f9a5cb3d82e055d31ee2b8d65277
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2023-06-22 17:11:34 -0500

    this works; remove generateToken if a token is not found now

--- simpleJsonApp/internal/auth/auth.go
+++ simpleJsonApp/internal/auth/auth.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"errors"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"strings"
@@ -13,18 +14,18 @@ import (
 
 var adminMap = map[string]string{"admin": "password"}
 
-func getHmacSecret() string {
+func getHmacSecret() []byte {
 	hmacSecret, isExist := os.LookupEnv("HMAC_SECRET")
 	if !isExist {
-		return ""
+		return []byte{}
 	}
 
-	return hmacSecret
+	return []byte(hmacSecret)
 }
 
 func GenerateToken() (string, error) {
 	hmacSecret := getHmacSecret()
-	if hmacSecret == "" {
+	if len(hmacSecret) == 0 {
 		return "", errors.New("environment variable HMAC_SECRET not set")
 	}
 
@@ -40,6 +41,7 @@ func GenerateToken() (string, error) {
 func ParseAndValidate(tokenString string) (bool, error) {
 	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
 		}
 
@@ -59,11 +61,23 @@ func ParseAndValidate(tokenString string) (bool, error) {
 func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		tokenValue := c.GetHeader("Authorization")
-		fmt.Println("token is ", tokenValue)
 		tokenSlice := strings.Split(tokenValue, "Bearer ")
-		token := tokenSlice[len(tokenSlice)-1]
+
+		var token string
+		if len(tokenSlice) == 1 {
+			tokenV, err := GenerateToken()
+			if err != nil {
+				c.AbortWithStatus(http.StatusInternalServerError)
+				log.Fatalf("fail to generate token, %v", err)
+			}
+			token = tokenV
+		} else {
+			token = tokenSlice[len(tokenSlice)-1]
+		}
 
 		valid, err := ParseAndValidate(token)
+
+		fmt.Println("valid is", valid, err)
 		if err != nil {
 			c.AbortWithStatus(http.StatusInternalServerError)
 			return
