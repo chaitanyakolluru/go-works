e7c7e7480868ddb2057e0eecdc529abe8a1b8efa
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2023-09-23 18:44:36 -0500

    adding another solution from chatgpt

--- valid-parentheses/main.go
+++ valid-parentheses/main.go
@@ -73,6 +73,28 @@ func isValid(s string) bool {
 	return paren1Count == 0 && paren2Count == 0 && paren3Count == 0
 }
 
+func isValidFromChatGPT(s string) bool {
+	parenMap := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	stack := make([]rune, 0)
+
+	for _, char := range s {
+		if char == '(' || char == '[' || char == '{' {
+			stack = append(stack, char)
+		} else if char == ')' || char == ']' || char == '}' {
+			if len(stack) == 0 || stack[len(stack)-1] != parenMap[char] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}
+
 func main() {
 	//fmt.Println(isValid("()[]{}"))
 	//	fmt.Println(isValid("([)]"))
