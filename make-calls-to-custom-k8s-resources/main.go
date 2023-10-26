b5c77b845268b57d2d373dcac22d98a61cb37888
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2023-10-25 19:15:16 -0500

    testing custom resource querying

--- make-calls-to-custom-k8s-resources/main.go
+++ make-calls-to-custom-k8s-resources/main.go
@@ -1,49 +1,32 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/client-go/dynamic"
-
 	utils "git.heb.com/kub/composition-functions/resources/utils"
-
 	psjav1alpha1 "git.heb.com/provider-simplejsonapp/apis/records/v1alpha1"
-
-	"k8s.io/client-go/rest"
 )
 
-func createClient() *dynamic.DynamicClient {
-	config, err := rest.InClusterConfig()
+func main() {
+	d, err := utils.NewDynamicClient(
+		utils.WithGroup("records.simplejsonapp.crossplane.io"),
+		utils.WithVersion("v1alpha1"),
+		utils.WithResource("records"),
+	)
+
 	if err != nil {
-		log.Fatalf("cannot retrieve in-cluster config, err: %s", err)
+		log.Fatalf("error when creating dynamic client, err: %s", err)
 	}
 
-	client, err := dynamic.NewForConfig(config)
+	result, err := utils.GetCustomResource(d, "example-record-new")
+
 	if err != nil {
-		log.Fatalf("cannot create dynamic client, err: %s", err)
-	}
-
-	return client
-}
-
-func getRecord(client *dynamic.DynamicClient, record string) {
-	recordRes := schema.GroupVersionResource{Group: "records.simplejsonapp.crossplane.io", Version: "v1alpha1", Resource: "records"}
-
-	result, err := client.Resource(recordRes).Get(context.TODO(), record, metav1.GetOptions{})
-	if err != nil {
-		panic(err)
+		log.Fatalf("error with getting custom resource, err: %s", err)
 	}
 
 	var r psjav1alpha1.Record
 	utils.Hydrate(result, &r)
 
-	fmt.Printf("record is: %s, spec.Location: %s", r.ObjectMeta.Name, r.Spec.ForProvider.Location)
-}
-
-func main() {
-	getRecord(createClient(), "example-record-new")
+	fmt.Printf("Using Utils: record is: %s, spec.Location: %s\n", r.ObjectMeta.Name, r.Spec.ForProvider.Location)
 }
