ed2c529fd61145cf90a31875991a1c64898b6b9a
Chaitanya Kolluru <chaitanya.r.kolluru@gmail.com>
2024-02-03 19:30:50 -0600

    adding in gorm

--- ums/main.go
+++ ums/main.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 	"net/http"
 
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
 	"github.com/labstack/echo/v4"
 )
 
 type UserData struct {
-	ID   int `json:"id"`
+	gorm.Model
 	User `json:"user"`
 }
 
@@ -19,66 +22,72 @@ type User struct {
 
 var users []UserData
 
-func saveUser(c echo.Context) error {
+type Context struct {
+	echo.Context
+	DB *gorm.DB
+}
+
+func saveUser(cc Context) error {
 	u := new(User)
-	if err := c.Bind(u); err != nil {
+	if err := cc.Bind(u); err != nil {
 		return err
 	}
 
 	for _, user := range users {
 		if user.User.Name == u.Name {
-			return c.JSON(http.StatusConflict, "user already exists")
+			return cc.JSON(http.StatusConflict, "user already exists")
 		}
 	}
 
-	users = append(users, UserData{ID: len(users) + 1, User: *u})
-	return c.JSON(http.StatusCreated, u)
+	cc.DB.Create(&UserData{User: *u})
+
+	return cc.JSON(http.StatusCreated, u)
 }
 
-func deleteUser(c echo.Context) error {
-	id := c.Param("id")
+func deleteUser(cc Context) error {
+	id := cc.Param("id")
 
 	for i, user := range users {
 		if id == fmt.Sprintf("%d", user.ID) {
 			users = append(users[:i], users[i+1:]...)
-			return c.JSON(http.StatusOK, id)
+			return cc.JSON(http.StatusOK, id)
 		}
 	}
 
-	return c.JSON(http.StatusNotFound, id)
+	return cc.JSON(http.StatusNotFound, "user deleted")
 }
 
-func updateUser(c echo.Context) error {
-	id := c.Param("id")
+func updateUser(cc Context) error {
+	id := cc.Param("id")
 	u := new(User)
-	if err := c.Bind(u); err != nil {
+	if err := cc.Bind(u); err != nil {
 		return err
 	}
 
 	for i, user := range users {
 		if id == fmt.Sprintf("%d", user.ID) {
 			users[i].User = *u
-			return c.JSON(http.StatusOK, users[i])
+			return cc.JSON(http.StatusOK, users[i])
 		}
 	}
 
-	return c.JSON(http.StatusNotFound, id)
+	return cc.JSON(http.StatusNotFound, "user not found")
 }
 
-func getUsers(c echo.Context) error {
-	return c.JSON(http.StatusOK, users)
+func getUsers(cc Context) error {
+	return cc.JSON(http.StatusOK, users)
 }
 
-func getUser(c echo.Context) error {
-	id := c.Param("id")
+func getUser(cc Context) error {
+	id := cc.Param("id")
 
 	for _, u := range users {
 		if id == fmt.Sprintf("%d", u.ID) {
-			return c.JSON(http.StatusOK, u)
+			return cc.JSON(http.StatusOK, u)
 		}
 	}
 
-	return c.JSON(http.StatusNotFound, id)
+	return cc.JSON(http.StatusNotFound, "user not found")
 }
 
 func healthCheck(c echo.Context) error {
@@ -86,7 +95,20 @@ func healthCheck(c echo.Context) error {
 }
 
 func main() {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	db.AutoMigrate(&UserData{})
 	e := echo.New()
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			cc := &Context{c, db}
+			return next(cc)
+		}
+	})
 
 	e.GET("/healthz", healthCheck)
 	e.POST("/users", saveUser)
